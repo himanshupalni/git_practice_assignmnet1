3170635bdf47b169856c9bdae1a58e6844d79e20
Your Namehimanshuupreti176@gmail.comcd <you@example.com>
2022-11-02 22:47:39 +0530

    code sahi kar diya

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,6 @@
-
-
-let string= "naman";
+let string= "racecar";
 let bag="";
-  for (let i=string.length-1;i<=0;i++){
+  for (let i=string.length-1;i>=0;i--){
    bag= bag+string[i];
   } 
      if(bag==string){
